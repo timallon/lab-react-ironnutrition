85d05c016f1494f75b9469dd9b9b22aa431d6d13
Tim Allon <timallon@gmail.com>
2022-11-16 14:48:41 +0000

    iteration 1 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import React, { useState } from 'react';
+import { Row, Divider, Button } from 'antd';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [food, setFood] = useState(foods);
+  return <div className="App">
+    <div>
+      {/* Display Add Food component here */}
 
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {food.map((food) => {
+          return (
+            <div>
+              <p> {food.name } </p>
+              <img src={food.image} width={100} />
+              
+            </div>
+            
+          )
+        })}
+      </Row>
+    </div>
+
+      
+
+
+  </div>
+
+}
 export default App;
